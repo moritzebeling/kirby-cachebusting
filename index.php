7360ab71dcee16af5f68b37dcb7c7d7866ab4b7e
moritzebeling <git@moritzebeling.com>
2020-10-31 18:53:14 +0100

    fix indentation

--- index.php
+++ index.php
@@ -4,34 +4,22 @@ use Kirby\Cms\App;
 use Kirby\Cms\Url;
 
 Kirby::plugin('moritzebeling/kirby-cachebusting', [
-
-	'components' => [
-
-		'css' => function (Kirby $kirby, string $url, $options = null): string {
-		  $relative_url = Url::path($url, false);
-          $file_root = $kirby->root('index') . DS . $relative_url;
-
-          if (F::exists($file_root)) {
-              return url($relative_url . '?' . F::modified($file_root));
-          }
-
-		  return $url;
-
-		},
-
-		'js' => function (Kirby $kirby, string $url, $options = null): string {
-
-			$relative_url = Url::path($url, false);
-			$file_root = $kirby->root('index') . DS . $relative_url;
-
-			if (F::exists($file_root)) {
-				return url($relative_url . '?' . F::modified($file_root));
-			}
-
-			return $url;
-
-		},
-
-	],
-
+  'components' => [
+    'css' => function (Kirby $kirby, string $url, $options = null): string {
+      $relative_url = Url::path($url, false);
+      $file_root = $kirby->root('index') . DS . $relative_url;
+      if (F::exists($file_root)) {
+        return url($relative_url . '?' . F::modified($file_root));
+      }
+      return $url;
+    },
+	'js' => function (Kirby $kirby, string $url, $options = null): string {
+      $relative_url = Url::path($url, false);
+      $file_root = $kirby->root('index') . DS . $relative_url;
+      if (F::exists($file_root)) {
+        return url($relative_url . '?' . F::modified($file_root));
+      }
+      return $url;
+    },
+  ],
 ]);
