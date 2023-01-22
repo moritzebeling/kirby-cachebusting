ad8dfb4b978f6b15f98a30a2d9251bc77d2fbd12
moritzebeling <git@moritzebeling.com>
2023-01-22 14:13:10 +0100

    update

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
-# Kirby Cachebusting
+# Kirby Cachebusting, CDN friendly
 
-Adds a fingerprint to referenced js and css files. Uses timestamp of last modification.
+⚠️ This is taken from the Kirby website and source code and only here to simplify installation for myself via composer. Plese check:
 
-Suggested by https://forum.getkirby.com/t/plugin-assets-timestamp/13975/27
+https://getkirby.com/docs/cookbook/extensions/kirby-loves-cdn#cache-busting
+https://github.com/getkirby/getkirby.com/blob/main/site/plugins/cdn/src/Cachebuster.php
 
-Issues and PRs welcome.
+The plugin adds a finderprint like `file.css?v=12345678` to every file added from the `/assets` folder using on of the Kirby helper methods like `css()` or `js()`.
 
-## Installation
+## Setup
 
-Clone as plugin into your Kirby installation
-```
-git clone https://github.com/moritzebeling/kirby-cachebusting.git site/plugins/kirby-cachebusting
-```
-As submodule
-```
-git submodule add https://github.com/moritzebeling/kirby-cachebusting.git site/plugins/kirby-cachebusting
-```
-Update submodules
-```
-git submodule foreach git pull origin master
+```php
+# site/config/config.php
+return [
+    'moritzebeling.kirby-cachebusting' => false // to switch off completely,
+    'moritzebeling.kirby-cachebusting.host' => 'https://remote-host.com', // to set remote host, like cdn
+    // ...
+];
 ```
--- index.php
+++ index.php
@@ -1,25 +1,62 @@
 <?php
 
+namespace Kirby\Cdn;
+
 use Kirby\Cms\App;
-use Kirby\Cms\Url;
+use Kirby\Cms\App as Kirby;
+use Kirby\Toolkit\Str;
+
+
+class Cachebuster {
+    
+    /*
+    https://github.com/getkirby/getkirby.com/blob/main/site/plugins/cdn/src/Cachebuster.php
+    */
+
+    protected static function version(string $root, string $path): string
+    {
+        return dechex(filemtime($root));
+    }
+
+    public static function path(string $path): string
+    {
+        if (strpos($path, url()) === 0) {
+            $path = ltrim(substr($path, strlen(url())), '/');
+        }
+
+        $kirby = App::instance();
+        $root  = $kirby->root('index') . '/' . $path;
+
+        if (file_exists($root)) {
+            $version = static::version($root, $path);
+            $path = $path . '?v=' . $version;
+        }
+
+        return $path;
+    }
+
+}
 
 Kirby::plugin('moritzebeling/kirby-cachebusting', [
-  'components' => [
-    'css' => function (Kirby $kirby, string $url, $options = null): string {
-      $relative_url = Url::path($url, false);
-      $file_root = $kirby->root('index') . DS . $relative_url;
-      if (F::exists($file_root)) {
-        return url($relative_url . '?' . F::modified($file_root));
-      }
-      return $url;
-    },
-    'js' => function (Kirby $kirby, string $url, $options = null): string {
-      $relative_url = Url::path($url, false);
-      $file_root = $kirby->root('index') . DS . $relative_url;
-      if (F::exists($file_root)) {
-        return url($relative_url . '?' . F::modified($file_root));
-      }
-      return $url;
-    },
-  ],
+    
+    /*
+    https://getkirby.com/docs/cookbook/extensions/kirby-loves-cdn#cache-busting
+    */
+
+    'options' => [
+        'host' => false
+    ],
+    'components' => [
+        'url' => function ($kirby, $path, $options) {
+            if (Str::startsWith($path, 'assets')) {
+                $path = Cachebuster::path($path);
+
+                if ( $host = option('moritzebeling.kirby-cachebusting.host', false) ){
+                    return $host . '/' . $path;
+                }
+            }
+            $original = $kirby->nativeComponent('url');
+            return $original($kirby, $path, $options);
+        },
+    ]
 ]);
